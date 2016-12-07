faf33b280a9782d9560bb904c53dc7cf913e7d2a
Martin KaFai Lau <kafai@fb.com>
2016-12-07 15:53:11 -0800

    bpf: add XDP prog type for early driver filter
    Add a new bpf prog type that is intended to run in early stages of the
    packet rx path. Only minimal packet metadata will be available, hence a
    new context type, struct xdp_md, is exposed to userspace. So far only
    expose the packet start and end pointers, and only in read mode.
    
    An XDP program must return one of the well known enum values, all other
    return codes are reserved for future use. Unfortunately, this
    restriction is hard to enforce at verification time, so take the
    approach of warning at runtime when such programs are encountered. Out
    of bounds return codes should alias to XDP_ABORTED.
    
    Signed-off-by: Brenden Blanco <bblanco@plumgrid.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -474,6 +474,12 @@ union bpf_attr {
  *     @flags: Flags (unused for now)
  *     Return: 0 on success or negative error
  *
+ * int bpf_xdp_adjust_head(xdp_md, delta)
+ *     Adjust the xdp_md.data by delta
+ *     @xdp_md: pointer to xdp_md
+ *     @delta: An positive/negative integer to be added to xdp_md.data
+ *     Return: 0 on success or negative on error
+ *
  * int bpf_probe_read_str(void *dst, int size, const void *unsafe_ptr)
  *     Copy a NUL terminated string from unsafe address. In case the string
  *     length is smaller than size, the target is not padded with further NUL
@@ -542,6 +548,7 @@ union bpf_attr {
 	FN(set_hash_invalid),		\
 	FN(get_numa_node_id),		\
 	FN(skb_change_head),		\
+	FN(xdp_adjust_head),		\
 	FN(probe_read_str),		\
 	FN(get_socket_cookie),		\
 	FN(get_socket_uid),
@@ -650,6 +657,8 @@ struct bpf_sock {
 	__u32 type;
 	__u32 protocol;
 };
+
+#define XDP_PACKET_HEADROOM 256
 
 /* User return codes for XDP prog type.
  * A valid XDP program must return one of these defined values. All other
