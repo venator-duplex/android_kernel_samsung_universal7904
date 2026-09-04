86295a653e952acc63ff27c473941b645c4a2e6a
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-04 10:16:29 +0800

    Update tfa_dsp.c

--- sound/soc/codecs/tfa9872/tfa_dsp.c
+++ sound/soc/codecs/tfa9872/tfa_dsp.c
@@ -817,7 +817,7 @@ tfa98xx_filter_mem(tfa98xx_handle_t dev,
 		case 0x72:
 		default:
 			/* unsupported case, possibly intermediate version */
-			return TFA98XX_ERROR;
+			return TFA98XX_DMEM_ERR;
 			_ASSERT(0);
 		}
 	}
@@ -942,7 +942,7 @@ enum tfa98xx_error tfa98xx_set_saam_use_case(int samstream)
 
 	if (devcount < 1) {
 		pr_err("No or wrong container file loaded\n");
-		return TFA98XX_ERROR;
+		return TFA98XX_ERROR_BAD_PARAMETER;
 	}
 
 	for (dev = 0; dev < devcount; dev++)
@@ -963,7 +963,7 @@ enum tfa98xx_error tfa98xx_set_stream_state(int stream_state)
 
 	if (devcount < 1) {
 		pr_err("No or wrong container file loaded\n");
-		return TFA98XX_ERROR;
+		return TFA98XX_ERROR_BAD_PARAMETER;
 	}
 
 	for (dev = 0; dev < devcount; dev++)
