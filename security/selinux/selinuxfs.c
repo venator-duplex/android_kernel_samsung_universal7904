6f72989374442cad721bbbda716979949f5f91cc
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-05 14:44:38 +0800

    Update selinuxfs.c

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -60,7 +60,7 @@ static int __init checkreqprot_setup(char *str)
 }
 __setup("checkreqprot=", checkreqprot_setup);
 
-static DEFINE_MUTEX(sel_mutex);
+DEFINE_MUTEX(sel_mutex);
 
 /* global data for booleans */
 static struct dentry *bool_dir;
