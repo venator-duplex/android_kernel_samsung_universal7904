cdd56485c928eb5d5b6c35696047b15cb88904a6
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-05 12:53:37 +0800

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -449,12 +449,28 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 
 #ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_sys_read(-1);  /* -1 表示非 fd 路径，或者传递 0 作为占位符 */
+		ksu_handle_vfs_read(&file, &buf, &count, &pos);
 #endif
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
-	/* ... 后续原有代码 ... */
+	if (!(file->f_mode & FMODE_CAN_READ))
+		return -EINVAL;
+	if (unlikely(!access_ok(VERIFY_WRITE, buf, count)))
+		return -EFAULT;
+
+	ret = rw_verify_area(READ, file, pos, count);
+	if (ret >= 0) {
+		count = ret;
+		ret = __vfs_read(file, buf, count, pos);
+		if (ret > 0) {
+			fsnotify_access(file);
+			add_rchar(current, ret);
+		}
+		inc_syscr(current);
+	}
+
+	return ret;
 }
 EXPORT_SYMBOL(vfs_read);
 
