f947b3299ebe0845668d5c9ac33de71abeee6a0f
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-05 20:11:21 +0800

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -569,7 +569,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
-#if 0 CONFIG_KSU_MANUAL_HOOK
+#if 0
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd);
 #endif
